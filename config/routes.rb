77f612cfd43c3b91dd2fa88b22b77545027d1dbd
abhishek-kr-01 <ak472872@gmail.com>
2020-08-26 03:03:15 +0530

    added react

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
-
-
+  # get 'homepage/index'
 	namespace 'api' do
 		namespace 'v1' do
-
 			resources :users
 			resources :meetings
 			resources :roles
-
 		end
 	end
-	# root 'homepage#index'
+	root 'homepage#index'
 end
