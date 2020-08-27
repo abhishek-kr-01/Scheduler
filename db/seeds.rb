2e3e31ea954cf56ea6c58a73ac61a17260343609
abhishek-kr-01 <ak472872@gmail.com>
2020-08-27 17:58:10 +0530

    added seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,18 @@
-# Role.create( {title: "candidate"} )
-# Role.create( {title: "recruiter"} )
-# 3.times do
-# 	User.create({
-# 		name: Faker::Name.name,
-# 		email: Faker::Internet.email,
-# 		role_id: 2
-# 	})
-# end
-# 10.times do
-# 	User.create({
-# 		name: Faker::Name.name,
-# 		email: Faker::Internet.email,
-# 		role_id: 1
-# 	})
-# end
+Role.create( {title: "candidate"} )
+Role.create( {title: "recruiter"} )
+3.times do
+	User.create({
+		name: Faker::Name.name,
+		email: Faker::Internet.email,
+		role_id: 2
+	})
+end
+10.times do
+	User.create({
+		name: Faker::Name.name,
+		email: Faker::Internet.email,
+		role_id: 1
+	})
+end
 
 
